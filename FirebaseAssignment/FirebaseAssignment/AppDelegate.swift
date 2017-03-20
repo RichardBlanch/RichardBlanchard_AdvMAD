954a7091eaba74f388b7e1d297c87536e60c0f00
Rich Blanchard <blanchard5595@yahoo.com>
2017-03-20 13:44:24 -0600

    Added Firebase Assignment

---
+++ FirebaseAssignment/FirebaseAssignment/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  FirebaseAssignment
+//
+//  Created by Rich Blanchard on 3/20/17.
+//  Copyright © 2017 Rich. All rights reserved.
+//
+
+import UIKit
+import Firebase
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Create a reference to a Firebase location
+         FIRApp.configure()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
